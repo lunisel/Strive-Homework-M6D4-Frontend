2b2eedc0043c145cf4edea7f23d05c102fd34a64
Luna Sarmiento <selene942008@hotmail.com>
2021-08-28 12:32:51 +0200

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,44 +1,83 @@
 import { Col, Container, Form, Row } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
-import { CaretDownFill } from "react-bootstrap-icons";
+import { Cup, Cart4 } from "react-bootstrap-icons";
+import UserPhoto from "./../assets/user.png";
 
-const Navbar = (props) => {
-  console.log(props);
+const Navbar = ({ history }) => {
   return (
     <div className="nav-container">
       <Container className="nav-items-cont">
-        <Row className="w-100">
-          <Col xs={8} md={4}>
+        <Row className="w-100 justify-content-between">
+          <Col xs={5} md={2}>
             <Link to="/" className="home-logo">
               LOGO
             </Link>
           </Col>
 
-          <Col md={4} className="d-none d-md-flex align-items-center">
+          <Col md={7} className="d-none d-md-flex align-items-center">
             <Form.Control type="text" placeholder="Search..."></Form.Control>
           </Col>
 
-          <Col xs={4} md={4} className="d-flex align-items-center">
+          <Col
+            xs={7}
+            md={3}
+            className="d-flex align-items-center justify-content-end"
+          >
             <div class="btn-group">
               <button
                 type="button"
-                class="btn btn-secondary dropdown-toggle"
+                className="btn dropdown-toggle p-0"
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
-              ></button>
-              <div class="dropdown-menu dropdown-menu">
-                <button class="dropdown-item" type="button">
+              >
+                <Cup
+                  viewBox="0 0 16 16"
+                  width={30}
+                  height={30}
+                  fill={"#f2c879"}
+                  className="cup"
+                />
+              </button>
+              <div className="dropdown-menu dropdown-menu">
+                <button
+                  className="dropdown-item"
+                  type="button"
+                  onClick={() => history.push("/categories/strong")}
+                >
                   Strong
                 </button>
-                <button class="dropdown-item" type="button">
+                <button
+                  className="dropdown-item"
+                  type="button"
+                  onClick={() => history.push("/categories/medium")}
+                >
                   Medium
                 </button>
-                <button class="dropdown-item" type="button">
+                <button
+                  className="dropdown-item"
+                  type="button"
+                  onClick={() => history.push("/categories/light")}
+                >
                   Light
                 </button>
               </div>
             </div>
+
+            <img
+              src={UserPhoto}
+              alt="user image"
+              className="user-img mx-3"
+              onClick={() => history.push("/account")}
+            />
+            <Cart4
+              viewBox="0 0 16 16"
+              width={30}
+              height={30}
+              fill={"#f2c879"}
+              onClick={() => history.push("/cart")}
+              className="cart"
+            />
           </Col>
         </Row>
       </Container>
